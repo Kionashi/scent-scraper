fa76ad75cddebc8262f364f7011aa534c2813816
Kionashi <cardozo.anibal@gmail.com>
2023-07-12 12:18:17 +0200

    feat: Add requirements -Define env variables

--- manage.py
+++ manage.py
@@ -2,10 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
+    
+    # Load environment variables from .env file
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scentscraper.settings')
     try:
         from django.core.management import execute_from_command_line
