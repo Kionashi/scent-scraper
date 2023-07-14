838b0ddcc95fadd4bef8a29aabb29741df98a946
Kionashi <cardozo.anibal@gmail.com>
2023-07-14 09:06:04 +0200

    feat: move db credentials to .env file

--- scentscraper/settings.py
+++ scentscraper/settings.py
@@ -79,9 +79,11 @@ WSGI_APPLICATION = 'scentscraper.wsgi.application'
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'scentscraper/mysql.cnf',
-        }
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     },
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
